25c829197e1ba48cf102f7e712e8cc762d8aadf5
burhanbadshah <burhanbadsha@gmail.com>
2021-12-29 13:54:31 +0530

    new express file

---
+++ burhan/app/app.js
@@ -0,0 +1,22 @@
+// require or call the express module
+var express = require("express");
+const { send } = require("express/lib/response");
+// calling the express sample constractoer
+var app = express();
+app.get("/", ()=>{
+
+});
+
+app.get("/about", (req, res)=>{
+    res.send("<h1>About page</h1>");
+});
+app.get("/contact", ()=>{
+    res.send("<h1>Contact Page</h1>");
+});
+app.get("/Help", ()=>{
+    res.send("<h1>Help page</h1>");
+});
+
+app.listen(7860,()=>{
+    console.log("server running");
+});
