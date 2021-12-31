b744fe5e209e56cffd78dfc4a8c572e563d5f3e1
mp0001625 <mp0001625@gmail>
2021-12-31 10:52:57 +0530

    mohit

--- Mohit/exp1/app.js
+++ Mohit/exp1/app.js
@@ -2,8 +2,23 @@ var express = require("express");
 
 var app = express();
 
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname+"/home.html");
+});
 
-app.listen(3000, ()=>{
+app.get("/about", (req, res)=>{
+    res.sendFile(__dirname+"/about.html");
+});
+
+app.get("/contact", (req, res)=>{
+    res.sendFile(__dirname+"/contact.html");
+});
+
+app.get("/help", (req, res)=>{
+    res.sendFile(__dirname+"/help.html");
+});
+
+app.listen(3001, ()=>{
     
     console.log("server running");
 });
