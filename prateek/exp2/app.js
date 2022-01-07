22cf780eefcc686a6444047a6072d8d649af96e4
Prateek1809 <sprateek773@gmail.com>
2022-01-07 11:41:34 +0530

    new ejs file

--- prateek/exp2/app.js
+++ prateek/exp2/app.js
@@ -1,5 +1,33 @@
 // require or call the express module
-var express = require("express");
 
-// calling the express module constuctor
+var express = require("express");
+// calling the express module constructor
 var app = express();
+
+// localhost:3000
+app.get("/", (req, res)=>{
+    res.send("<h1>Hello World</h1>");
+})
+
+// localhost:3000/about
+
+app.get("/about", (req, res)=>{
+    res.send("<h1>About Page</h1>");
+});
+// localhost:3000/contact
+
+app.get("/contact", (req, res)=>{
+    res.send("<h1>Contact Page</h1>");
+
+
+});
+
+app.get("/help", (req, res)=>{
+    res.send("<h1>Help Page</h1>");
+});
+
+
+// create our virtual server with 3000 port
+app.listen(3000, ()=>{
+    console.log("server running");
+});
