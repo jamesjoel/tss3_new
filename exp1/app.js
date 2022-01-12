da023f0de0c48db29ec5a43da42ccc2ccbbce3d4
jamesjoel <jamesjoelj@gmail.com>
2022-01-12 15:55:18 +0530

    find in mongodb

--- exp1/app.js
+++ exp1/app.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
 
-// console.log(app);
+
 
 app.listen(3000, ()=>{
     console.log("server running");
+    console.log("welcome");
 });
+
 
 
