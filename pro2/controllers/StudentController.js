afa5a9e0147acf778e30f37b3454ee2abc20001e
jamesjoel <jamesjoelj@gmail.com>
2022-01-14 16:07:41 +0530

    import and edit

--- pro2/controllers/StudentController.js
+++ pro2/controllers/StudentController.js
@@ -5,8 +5,27 @@ const MongoClient = mongodb.MongoClient;
 
 
 routes.get("/", (req, res)=>{
-    var pageData = { pagename : "student", title : "Student"};
-    res.render("layout", pageData);
+    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+        var db = con.db("tss3");
+        db.collection("city").find().toArray((err, data)=>{
+            // console.log(data);
+            // var arr=[];
+            // for(var x in data[0]){
+            //     var temp = data[0][x];
+            //     if(isArray(temp)){
+
+                    
+            //         temp.forEach((x)=>{
+            //             arr.push(x);
+            //         })
+            //     }
+            // }
+            // console.log(arr);
+            // return;
+            var pageData = { pagename : "student", title : "Student", data : data};
+            res.render("layout", pageData);
+        })
+    })
 })
 
 routes.get("/view", (req, res)=>{
@@ -73,6 +92,32 @@ routes.post("/save", (req, res)=>{
         
 
     });
+})
+
+routes.get("/edit/:a", (req, res)=>{
+    var id = req.params.a;
+    var objid = mongodb.ObjectId(id);
+    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+        var db = con.db("tss3");
+        db.collection("student").find({ _id : objid }).toArray((err, data)=>{
+            var pageData = { pagename : "edit", title : "Edit", data : data[0]};
+            res.render("layout", pageData);
+        })
+    })
+
+})
+
+
+
+routes.post("/update/:a", (req, res)=>{
+    var id = req.params.a;
+    var objid = mongodb.ObjectId(id);
+    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+        var db = con.db("tss3");
+        db.collection("student").updateMany({ _id : objid }, { $set : req.body }, ()=>{
+            res.redirect("/student/view");
+        })
+    })
 })
 
 module.exports = routes;
