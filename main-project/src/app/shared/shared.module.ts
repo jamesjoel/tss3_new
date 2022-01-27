69e87042d560afe42c10a42f817889ea05e677c8
jamesjoel <jamesjoelj@gmail.com>
2022-01-27 14:51:09 +0530

    new project

---
+++ main-project/src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SliderComponent } from './slider/slider.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports : [
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent
+  ]
+})
+export class SharedModule { }
---
+++ main-project/src/app/user/user.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { UserRoutingModule } from './user-routing.module';
+import { UserComponent } from './user.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ServiceComponent } from './shared/service/service.component';
+import { ReservationComponent } from './shared/reservation/reservation.component';
+import { MasterChefComponent } from './shared/master-chef/master-chef.component';
+
+
+@NgModule({
+  declarations: [
+    UserComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServiceComponent,
+    ReservationComponent,
+    MasterChefComponent
+  ],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    SharedModule
+  ]
+})
+export class UserModule { }
