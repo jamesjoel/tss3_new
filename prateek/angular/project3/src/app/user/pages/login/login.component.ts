9ddf7ee1c6ff9566d5ed91920b66177b35bf8838
Prateek1809 <sprateek773@gmail.com>
2022-02-16 13:50:46 +0530

    validation

--- prateek/angular/project3/src/app/user/pages/login/login.component.ts
+++ prateek/angular/project3/src/app/user/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service'; 
 
 @Component({
@@ -11,10 +12,12 @@ export class LoginComponent implements OnInit {
 
   loginFrm : FormGroup;
   checkFrm = false;
+  errMsg = "";
 
   constructor(
     private _fb : FormBuilder,
-    private _auth : AuthService
+    private _auth : AuthService,
+    private _router : Router
   ) { 
     this.loginFrm = this._fb.group({
       username : ["", Validators.required],
@@ -30,9 +33,22 @@ export class LoginComponent implements OnInit {
       this.checkFrm=true;
       return;
     }
-    // console.log(this.loginFrm.value)
-    this._auth.do_login(this.loginFrm.value).subscribe(data=>{
-      console.log(data);
+    //console.log(this.loginFrm.value)
+    this._auth.do_login(this.loginFrm.value).subscribe(data => {
+      // console.log(data);
+      localStorage.setItem("token", data.token);
+      this._router.navigate(["/profile"]);
+     
+    }, err => {
+      // console.log(err.error);
+      if (err.error.type == 1) {
+        this.errMsg = "This Username/Email is not Registred !";
+      }
+      if (err.error.type == 2) {
+        this.errMsg = "This Password is Incorrect !";
+
+      }
     })
   }
+
 }
--- prateek/project-api/controllers/userAuthController.js
+++ prateek/project-api/controllers/userAuthController.js
@@ -4,13 +4,36 @@ let routes = express.Router();
 let mongodb = require("mongodb");
 let MongoClient = mongodb.MongoClient;
 let database = require("../config/database");
-let collName = 
+let collName = "user";
+let sha1 = require("sha1");
+let jwt = require("jsonwebtoken");
 
 routes.post("/", (req, res)=>{
-    // console.log(req.body);
-    var u =req.body.username;
-    var p =req.body.password;
+    var u = req.body.username;
+    var p = req.body.password;
+    MongoClient.connect(database.dbUrl, (err, con)=>{
+        var db = con.db(database.dbName);
+        db.collection(collName).find({ email : u }).toArray((err, result)=>{
+            if(result.length == 1)
+            {
+                if(result[0].password == sha1(p))
+                {
+                    var token = jwt.sign({ id : result[0]._id}, database.uniqueStr);
+                    console.log(token);
+                    res.status(200).json({ success : true, token : token });
+                }
+                else{
+                    res.status(401).json({ success: false, type: 2 });
+                }
+            }
+            else{
+                res.status(401).json({ success : false, type : 1 });
+            }
+        })
+    })
 
+
+    
     
 
 })
