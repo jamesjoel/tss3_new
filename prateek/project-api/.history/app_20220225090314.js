673b2d71ab438672d909580ccf28628bfe3a3ee1
Prateek1809 <sprateek773@gmail.com>
2022-03-04 13:23:29 +0530

    file

---
+++ prateek/project-api/.history/app_20220225090314.js
@@ -0,0 +1,50 @@
+let express = require("express");
+let app = express();
+let cors = require("cors");
+let routes = require("./config/routes");
+let upload = require("express-fileupload");
+const { MongoClient } = require("mongodb");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname+"/assets"));
+app.use(cors());
+app.use(upload());
+app.use(routes);
+
+app.get("/api/getimages", (req, res)=>{
+    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+        var db = con.db("tss3");
+        db.collection("images").find().toArray((err, result)=>{
+            // res.send(result);
+            var newresult = result.map(x=>{
+                x.imagepath = "http://localhost:3000/users/"+x.imagename;
+                return x;
+            })
+            res.send(newresult);
+        })
+    })
+})
+
+app.post("/api/upload", (req, res)=>{
+    // console.log(req.files);
+    var file = req.files.myfile;
+    // console.log(file);
+    var name = file.name;
+    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+        var db = con.db("tss3");
+        db.collection("images").insertOne({ imagename : name }, ()=>{
+            file.mv(__dirname + "/assets/users/" + name, (err) => {
+                res.send({ _id : "", imagename : name, imagepath: "http://localhost:3000/users/" + name });
+
+            });
+        })
+    })
+    
+})
+
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("server running with port ", port);
+})
