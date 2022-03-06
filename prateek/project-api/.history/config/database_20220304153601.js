6a1dbc30f6243f11a4ce676528082583b2c7186b
Prateek1809 <sprateek773@gmail.com>
2022-03-07 00:42:52 +0530

    pro

---
+++ prateek/project-api/.history/config/database_20220304153601.js
@@ -0,0 +1,5 @@
+module.exports = {
+    dbName :"tss3",
+    dbUrl : "mongodb+srv://prateek:shukla1809@cluster0.m8fev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    uniqueStr : "tss"
+}
