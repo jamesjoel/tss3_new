0d079d20eb141bf67b4ad805eb04b813139bc303
Prateek1809 <sprateek773@gmail.com>
2022-03-14 13:13:46 +0530

    interpollation

--- tss-admin/src/app/pages/enquaries/enquaries.component.ts
+++ tss-admin/src/app/pages/enquaries/enquaries.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-enquaries',
@@ -7,55 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnquariesComponent implements OnInit {
 
-  students = [
-    {
-      name : "Rohit",
-      id : "45",
-      gender : "male",
-      date : "03/05/2022",
-      status : "pending",
-      class : "warning",
-      image : "assets/images/faces/face1.jpg"
-    },
-    {
-      name : "Jaya",
-      id : "23",
-      gender : "female",
-      date : "11/03/2022",
-      status : "Approved",
-      class : "success",
-      image : "assets/images/faces/face2.jpg"
-    },
-    {
-      name : "Nidhi",
-      id : "13",
-      gender : "female",
-      date : "25/03/2022",
-      status : "Approved",
-      class : "success",
-      image : "assets/images/faces/face5.jpg"
-    },
-    {
-      name : "Gaurav",
-      id : "36",
-      gender : "male",
-      date : "09/03/2022",
-      status : "Denied",
-      class : "danger",
-      image : "assets/images/faces/face3.jpg"
-    },
-    {
-      name : "Ram",
-      id : "67",
-      gender : "male",
-      date : "30/03/2022",
-      status : "Approved",
-      class : "success",
-      image : "assets/images/faces/face4.jpg"
-    },
-  ]
+  allstudents:any[]=[];
 
-  constructor() { }
+  constructor(
+    private _student : StudentService
+  ) {
+    this._student.getData().subscribe(data=>{
+      this.allstudents = data;
+    })
+   }
 
   ngOnInit(): void {
   }
